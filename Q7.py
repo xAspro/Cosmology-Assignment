10079cf26e5b4faae57107ec7699b4d6ca808e8d
Sooryabalan M <sooryabalan.m@tifr.res.in>
2023-12-26 01:40:22 +0530

    Error correction

--- Q7.py
+++ Q7.py
@@ -8,18 +8,18 @@ k=1.38*10**-23
 T=2.7255
 
 def B(v,T,z):
-    return (2*h*v**3)/c**2*1/(np.exp((h*v)/(k*T))-1)
+    return (2*h*(v/(1+z))**3)/c**2*1/(np.exp((h*v)/(k*T))-1)
     
 p=(np.linspace(7,15,50))
 v=np.power(10,p)
 for i in range(len(z)):
     print("z[i]= ",z[i])
-    plt.plot(v,B(v,T,z[i]),'x-',label=f"for z={z[i]}")
+    plt.plot(v,B(v,T,z[i]),label=f"for z={z[i]}")
 
 plt.title("BB radiation Spectrum of the Universe")
 plt.legend()
 plt.xlabel("frequency(Hz)")
 plt.ylabel("Radiation spectral density")
-# plt.xscale("log")
-# plt.yscale("log")
+plt.xscale("log")
+plt.yscale("log")
 plt.show()
